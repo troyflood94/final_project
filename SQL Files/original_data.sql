82188b6c171e560f3569ad45a6e312628faf0f3d
vidyavelusamy <vidyavelusamy@gmail.com>
2024-10-10 20:04:42 -0400

    sql files

---
+++ SQL Files/original_data.sql
@@ -0,0 +1,19 @@
+drop table Original_Data
+CREATE TABLE Original_Data (
+    SK_ID_CURR INT Primary key Not null,
+TARGET bool  ,
+NAME_CONTRACT_TYPE  varchar ,
+CODE_GENDER varchar ,
+FLAG_OWN_CAR varchar ,
+FLAG_OWN_REALTY varchar ,
+CNT_CHILDREN int ,
+AMT_INCOME_TOTAL float ,
+AMT_CREDIT float ,
+AMT_ANNUITY float ,
+AMT_GOODS_PRICE float ,
+NAME_INCOME_TYPE varchar ,
+NAME_EDUCATION_TYPE varchar ,
+NAME_HOUSING_TYPE varchar ,
+DAYS_EMPLOYED int,
+OCCUPATION_TYPE varchar
+);
